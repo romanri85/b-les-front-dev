b915c384d0200f0e650bf55111ab562cc7e7c607
romanri85 <romanri85@gmail.com>
2023-09-25 14:23:32 +0300

    fix: fix formkit-slider

--- components/pop-ups/ImageModal.vue
+++ components/pop-ups/ImageModal.vue
@@ -10,31 +10,31 @@
   <!--  </div>-->
   <TransitionRoot appear :show="isOpen" as="template">
     <Dialog as="div" class="relative z-50" @close="closeModal">
-      <TransitionChild
-          as="template"
-          enter="duration-300 ease-out"
-          enter-from="opacity-0"
-          enter-to="opacity-100"
-          leave="duration-200 ease-in"
-          leave-from="opacity-100"
-          leave-to="opacity-0"
-      >
+<!--      <TransitionChild-->
+<!--          as="template"-->
+<!--          enter="duration-300 ease-out"-->
+<!--          enter-from="opacity-0"-->
+<!--          enter-to="opacity-100"-->
+<!--          leave="duration-200 ease-in"-->
+<!--          leave-from="opacity-100"-->
+<!--          leave-to="opacity-0"-->
+<!--      >-->
         <div class="fixed inset-0 bg-black bg-opacity-25"/>
-      </TransitionChild>
+<!--      </TransitionChild>-->
 
       <div class="fixed inset-0 overflow-y-auto">
         <div
             class="flex min-h-full items-center justify-center p-4 text-center"
         >
-          <TransitionChild
-              as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0 scale-95"
-              enter-to="opacity-100 scale-100"
-              leave="duration-200 ease-in"
-              leave-from="opacity-100 scale-100"
-              leave-to="opacity-0 scale-95"
-          >
+<!--          <TransitionChild-->
+<!--              as="template"-->
+<!--              enter="duration-300 ease-out"-->
+<!--              enter-from="opacity-0 scale-95"-->
+<!--              enter-to="opacity-100 scale-100"-->
+<!--              leave="duration-200 ease-in"-->
+<!--              leave-from="opacity-100 scale-100"-->
+<!--              leave-to="opacity-0 scale-95"-->
+<!--          >-->
             <DialogPanel
                 :class="{
     'md:w-1/2 lg:w-1/3 w-full': isTallImage,
@@ -72,7 +72,7 @@
                 <buttons-primary-button-small @click="closeModal"><h3>Закрыть</h3></buttons-primary-button-small>
               </div>
             </DialogPanel>
-          </TransitionChild>
+<!--          </TransitionChild>-->
         </div>
       </div>
     </Dialog>
