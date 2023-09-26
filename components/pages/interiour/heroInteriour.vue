4ffddf0efd0ee3f784ad6b8a2e24dd2fd68ce8f0
romanri85 <romanri85@gmail.com>
2023-09-26 14:37:35 +0300

    feat: load text after images

--- components/pages/interiour/heroInteriour.vue
+++ components/pages/interiour/heroInteriour.vue
@@ -11,6 +11,12 @@ const props = defineProps({
   buttons: Object,
 })
 
+const imageLoaded = ref(false);  // Create a ref to hold the image load state
+
+const handleImageLoad = () => {
+  imageLoaded.value = true; // Update the ref once the image has loaded
+};
+
 
 
 </script>
@@ -19,32 +25,32 @@ const props = defineProps({
   <div
       class="xl:mt-[-100px] lg:mt-[-112px] md:mt-[-65px] mt-[-65px] w-screen h-[300px] md:h-[300px] lg:h-[560px]">
     <div class="text-white absolute left-1/2 lg:top-[180px] md:top-[120px] top-[120px] -translate-x-1/2">
-      <h1 class="hidden md:block text-center whitespace-nowrap text-shadow">{{$t(props.heroName)}}</h1>
-      <h2 class="md:hidden text-center whitespace-nowrap text-shadow">{{$t(props.heroName)}}</h2>
-      <h6  class="hidden md:block w-[90vw] md:w-[500px] text-white text-shadow text-center pt-6">{{$t(props.heroDescription)}}</h6>
+      <h1 v-if="imageLoaded" class="hidden md:block text-center whitespace-nowrap text-shadow">{{$t(props.heroName)}}</h1>
+      <h2 v-if="imageLoaded" class="md:hidden text-center whitespace-nowrap text-shadow">{{$t(props.heroName)}}</h2>
+      <h6 v-if="imageLoaded"  class="hidden md:block w-[90vw] md:w-[500px] text-white text-shadow text-center pt-6">{{$t(props.heroDescription)}}</h6>
       <div v-if="props.buttons"  class="flex gap-10 justify-center">
         <NuxtLink :to="props.buttons.searchPicByTag.link">
           <primary-button-bigger :class="{'underline': route.path === '/search-tags'}" class="hidden md:block">
-            <h2 class="text-shadow">{{ props.buttons.searchPicByTag.name }}</h2>
+            <h2 v-if="imageLoaded" class="text-shadow">{{ props.buttons.searchPicByTag.name }}</h2>
           </primary-button-bigger>
           <primary-button-bigger :class="{'underline': route.path === '/search-tags'}" class="md:hidden pt-6 md:pt-0">
-            <h3 class="text-shadow">{{ props.buttons.searchPicByTag.name }}</h3>
+            <h3 v-if="imageLoaded" class="text-shadow">{{ props.buttons.searchPicByTag.name }}</h3>
           </primary-button-bigger>
         </NuxtLink>
 
         <NuxtLink :to="props.buttons.projects.link">
           <primary-button-bigger :class="{'underline': route.path === '/interior'}" class="hidden md:block">
-            <h2 class="text-shadow">{{ props.buttons.projects.name }}</h2>
+            <h2 v-if="imageLoaded" class="text-shadow">{{ props.buttons.projects.name }}</h2>
           </primary-button-bigger>
           <primary-button-bigger :class="{'underline': route.path === '/interior'}" class="md:hidden pt-6 md:pt-0">
-            <h3 class="text-shadow">{{ props.buttons.projects.name }}</h3>
+            <h3 v-if="imageLoaded" class="text-shadow">{{ props.buttons.projects.name }}</h3>
           </primary-button-bigger>
         </NuxtLink>
 
       </div>
 
     </div>
-    <nuxt-img :src="props.heroImage" class="w-full h-full object-cover object-right"></nuxt-img>
+    <nuxt-img @load="handleImageLoad" :src="props.heroImage" class="w-full h-full object-cover object-right"></nuxt-img>
   </div>
 
 </template>
