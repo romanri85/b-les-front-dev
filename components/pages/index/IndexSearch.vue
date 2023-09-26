f2813fe4e44a1cbf5a0b2f869b84daa0cf999ab8
romanri85 <romanri85@gmail.com>
2023-09-26 11:38:20 +0300

    fix: fix bugs

--- components/pages/index/IndexSearch.vue
+++ components/pages/index/IndexSearch.vue
@@ -4,7 +4,7 @@
 
     <div class="lg:w-[50vw] w-[80vw] text-center flex flex-col justify-between items-center">
       <h2 class="text-25-mono mb-7">{{ $t('searchByParameters') }}</h2>
-      <div class="lg:block flex flex-wrap">
+      <div class="lg:block text-left flex flex-wrap">
         <div v-for="parameter in parameters" :key="parameter.name" class="py-2">
           <NuxtLink :to="{ path: '/catalog', query: { filters: parameter.id } }" class="lg:pb-4 pb-5 lg:pr-0 pr-7">
             <h3 class="inline-block underline-direction">{{ parameter.name }}</h3>
@@ -17,7 +17,7 @@
       <div
           class=" bg-[url('/parameters-search.webp')] bg-cover bg-center lg:w-2/3 w-full h-full flex justify-center items-center main-container">
         <buttons-secondary-button class="md:w-64 md:h-20 w-full h-16
-                md:bg-white sm:bg-primaryDark md:text-black text-white cursor-pointer">
+                md:bg-white bg-black md:text-black text-white cursor-pointer">
           <nuxt-link to="/search-tags">
           {{ $t('searchPhotoByTag') }}
           </nuxt-link>
