4dc4bccd31ff43f65a4f8a0ffcd0259bfde2cd41
romanri85 <romanri85@gmail.com>
2023-09-26 17:37:28 +0300

    feat: load text after images

--- app.vue
+++ app.vue
@@ -1,21 +1,6 @@
 <template>
-  <div class="h-screen">
-    <div
-        v-if="loading"
-        class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500"
-    ></div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtLoadingIndicator duration="5000" color="#348feb" height="10" />
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script setup lang="ts">
-const nuxtApp = useNuxtApp();
-const loading = ref(false);
-nuxtApp.hook("page:start", () => {
-  loading.value = true;
-});
-nuxtApp.hook("page:finish", () => {
-  loading.value = false;
-});
-</script>
