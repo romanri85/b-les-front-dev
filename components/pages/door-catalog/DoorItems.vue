9f4f7e1c99cfca930fbe2350f3e8d1e2c17d2432
romanri85 <romanri85@gmail.com>
2023-10-10 20:28:04 +0300

    fix: remove stickySidebar from mobile

--- components/pages/door-catalog/DoorItems.vue
+++ components/pages/door-catalog/DoorItems.vue
@@ -5,24 +5,26 @@ import {useAutoAnimate} from '@formkit/auto-animate/vue'
 import {useFiltersStore} from "~/stores/filtersStore";
 import {storeToRefs} from "pinia";
 import {useViewportSize} from "~/composables/useViewportSize";
-import {ref} from "vue";
 
 const filtersStore = useFiltersStore()
 
-const {total, pagesCount, products, page_size,page} = storeToRefs(filtersStore)
+const {total, pagesCount, products, page_size, page} = storeToRefs(filtersStore)
 // filtersStore.fetchProducts()
-
-
-// const [parent] = useAutoAnimate()
-
 const viewport = useViewportSize()
 
 
+// const parent = ref(null)
+
+const parent= ref(null)
+if (viewport.isDesktop || viewport.isTablet) {
+
+  [parent.value] = useAutoAnimate()
+}
+
+
 const scrollToDoorsBlock = () => {
-  // parent.value.scrollIntoView({ behavior: 'smooth' });
+  parent.value.scrollIntoView({behavior: 'smooth'});
 };
-
-
 
 
 // const page = ref(1)
@@ -32,12 +34,11 @@ function onChangePage(page) {
 
   if (viewport.isDesktop || viewport.isTablet) {
     scrollToDoorsBlock()
+  } else {
+    window.scrollTo(0, 380)
+
   }
-  // else{
-  //   window.scrollTo(0, 380)
-  //
-  // }
-    filtersStore.onChangeFilters({page: filtersStore.page})
+  filtersStore.onChangeFilters({page: filtersStore.page})
 
 
   // if(viewport.isMobile && filtersStore.activeFilters.page > 1){
@@ -53,20 +54,18 @@ function onChangePage(page) {
 }
 
 
-
-
-
 </script>
 
 <template>
   <div class="md:pl-5 lg:pl-16">
-    <div  class="text-primaryDark flex justify-center md:justify-between">
+    <div class="text-primaryDark flex justify-center md:justify-between">
       <sorting/>
-      <div  class="hidden md:block">
-      <p class=" mt-14  md:mt-0 font-regular" >Всего дверей: {{ filtersStore.total }}</p>
+      <div class="hidden md:block">
+        <p class=" mt-14  md:mt-0 font-regular">Всего дверей: {{ filtersStore.total }}</p>
       </div>
     </div>
-    <div  class="mt-4 md:mt-16  md:gap-y-8 lg:grid-cols-4 mdLg:grid-cols-3 md:grid-cols-2 grid-cols-1 grid-rows-7 grid" ref="parent">
+    <div class="mt-4 md:mt-16  md:gap-y-8 lg:grid-cols-4 mdLg:grid-cols-3 md:grid-cols-2 grid-cols-1 grid-rows-7 grid"
+         ref="parent">
 
       <div v-for="doorVariant in filtersStore.products" :key="doorVariant.id">
         <NuxtLink
@@ -79,10 +78,10 @@ function onChangePage(page) {
     </div>
     <div class="w-full flex justify-center">
       <base-pagination class="pb-32" :total="filtersStore.total"
-                  :page_size="filtersStore.page_size"
-                  :pagesCount="filtersStore.pagesCount"
-                  @page-change="onChangePage"
-                  v-model:current-page="filtersStore.page"
+                       :page_size="filtersStore.page_size"
+                       :pagesCount="filtersStore.pagesCount"
+                       @page-change="onChangePage"
+                       v-model:current-page="filtersStore.page"
       />
     </div>
   </div>
