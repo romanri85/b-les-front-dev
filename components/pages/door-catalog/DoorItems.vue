d65378f1acd7c34211dd17adc79409cc9091376b
romanri85 <romanri85@gmail.com>
2023-10-10 17:19:49 +0300

    fix: remove stickySidebar from mobile

--- components/pages/door-catalog/DoorItems.vue
+++ components/pages/door-catalog/DoorItems.vue
@@ -30,14 +30,14 @@ const scrollToDoorsBlock = () => {
 function onChangePage(page) {
   filtersStore.page = page
 
-  if (viewport.isDesktop || viewport.isTablet) {
+  // if (viewport.isDesktop || viewport.isTablet) {
     scrollToDoorsBlock()
-  }
-  else{
-
-    window.scrollTo(0, 380)
-
-  }
+  // }
+  // else{
+  //
+  //   window.scrollTo(0, 380)
+  //
+  // }
   filtersStore.onChangeFilters({page: filtersStore.page})
 
   // if(viewport.isMobile && filtersStore.activeFilters.page > 1){
