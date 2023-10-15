2c85dcbc83a32f9c17e8e237699ddd2d27db8196
romanri85 <romanri85@gmail.com>
2023-10-15 14:12:50 +0300

    feat: add sentry to the project

--- plugins/sentry.plugin.js
+++ plugins/sentry.plugin.js
@@ -7,11 +7,29 @@ export default defineNuxtPlugin((nuxtApp) => {
         app: [vueApp],
         dsn: 'https://ab7e48b8a9a0b5939675a7eda3081d97@o4506039434805248.ingest.sentry.io/4506039440441344>',
         environment: process.dev ? 'development' : 'production',
+        // integrations: [
+        //     new BrowserTracing({
+        //         routingInstrumentation: Sentry.vueRouterInstrumentation(nuxtApp.$router)
+        //     })
+        // ],
+        // // Set tracesSampleRate to 1.0 to capture 100%
+        // // of transactions for performance monitoring.
+        // // We recommend adjusting this value in production
+        // tracesSampleRate: 1.0,
+        // beforeSend (event, hint) {
+        //     // Check if it is an exception, and if so, log it.
+        //     if (event.exception) {
+        //         // eslint-disable-next-line no-console
+        //         console.error(`[Exeption handled by Sentry]: (${hint.originalException})`, { event, hint });
+        //     }
+        //     // Continue sending to Sentry
+        //     return event;
+        // }
         integrations: [
             new BrowserTracing({
                 routingInstrumentation: Sentry.vueRouterInstrumentation(nuxtApp.$router),
             }),
-            new Sentry.Replay(),
+            // new Sentry.Replay(),
         ],
 
         // Set tracesSampleRate to 1.0 to capture 100%
